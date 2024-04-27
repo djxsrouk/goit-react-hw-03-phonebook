74a2f12d55b2f7ebf438c856e8f1d5cb3bef81d5
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-04-27 14:56:54 +0100

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import React from "react";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div class="main">
+      Wello world!
     </div>
   );
 };
